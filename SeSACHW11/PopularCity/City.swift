5c1971b7f83ed5f1485321a3b3a1f4e62e180f93
morgan4563 <morganpark4563@gmail.com>
2025-07-15 21:22:37 +0900

    feat: CityInfo 데이터 추가 City Model 구현

---
+++ SeSACHW11/PopularCity/City.swift
@@ -0,0 +1,16 @@
+//
+//  City.swift
+//  SeSACHW11
+//
+//  Created by hyunMac on 7/15/25.
+//
+
+import Foundation
+
+struct City {
+    let city_name: String
+    let city_english_name: String
+    let city_explain: String
+    let city_image: String
+    let domestic_travel: Bool
+}
