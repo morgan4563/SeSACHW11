3e48fa7e42c75b816ab1c2cc5bf9a27b0651ee4a
morgan4563 <morganpark4563@gmail.com>
2025-07-15 17:06:10 +0900

    fix: TableViewController로 설정하여 발생한 오류 ViewController로 변경하여 수정

---
+++ SeSACHW11/Travel/TravelADDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TravelADDetailViewController.swift
+//  SeSACHW11
+//
+//  Created by hyunMac on 7/15/25.
+//
+
+import UIKit
+
+class TravelADDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
