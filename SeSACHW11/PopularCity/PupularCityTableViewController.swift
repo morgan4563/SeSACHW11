053a3ef116a292bf9057c1d4e37d311ce9c888f0
morgan4563 <morganpark4563@gmail.com>
2025-07-16 00:24:16 +0900

    feat: 세그먼트 인덱스에 맞는 테이블 셀 목록 보여주기 구현

--- SeSACHW11/PopularCity/PupularCityTableViewController.swift
+++ SeSACHW11/PopularCity/PupularCityTableViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class PupularCityTableViewController: UITableViewController {
     let cities = CityInfo()
+    var filteredCities: [City] = []
+    @IBOutlet var segmentControl: UISegmentedControl!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         configureNib()
+        filteredCities = cities.city
+        segmentControl.addTarget(self, action: #selector(didChangeValue(segment:)), for: .valueChanged)
+
     }
 
     func configureNib() {
@@ -22,18 +27,30 @@ class PupularCityTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return cities.city.count
+        return filteredCities.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "PupularCityTableViewCell", for: indexPath) as! PupularCityTableViewCell
-        cell.configureUI(row: cities.city[indexPath.row])
-        
+        cell.configureUI(row: filteredCities[indexPath.row])
+
         return cell
     }
 
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return UITableView.automaticDimension
+    }
+
+    @objc func didChangeValue(segment: UISegmentedControl) {
+        switch segment.selectedSegmentIndex {
+        case 1:
+            filteredCities = cities.city.filter { $0.domestic_travel == true }
+        case 2:
+            filteredCities = cities.city.filter { $0.domestic_travel == false }
+        default:
+            filteredCities = cities.city
+        }
+        tableView.reloadData()
     }
 
 }
