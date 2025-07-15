f03c025df165177a2187341aa90720574fd467ac
morgan4563 <morganpark4563@gmail.com>
2025-07-15 16:12:15 +0900

    feat: HW13 storyboard 레아이웃/셀, 생성, 컨트롤러 연결

---
+++ SeSACHW11/PopularCity/PupularCityTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PupularCityTableViewCell.swift
+//  SeSACHW11
+//
+//  Created by hyunMac on 7/15/25.
+//
+
+import UIKit
+
+class PupularCityTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
